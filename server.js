df77b5f54cde16d11d0101dcde480dd1ad189cab
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2022-04-07 19:30:23 -0600

    removes test script form package.json

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-import express from "express"
+import express from "express";
 
-import memoryGameRouter from "./routes/memory-game-routes.js"
+import memoryGameRouter from "./routes/memory-game-routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(memoryGameRouter);
-
 
 app.listen(PORT, () => {
   console.log(`Server live on port ${PORT}`);
